a65d798c1da28e95986eaf48247a4563107b9920
Claudio Aleluia <henriqwe96@gmail.com>
2024-03-06 20:28:09 -0300

    feat: add pdParagraphsMd and blockeds elements to edit blur style

--- Content.js
+++ Content.js
@@ -1,11 +1,13 @@
 setInterval(() => {
+  // Elements with to remove
   const paywalls = document.getElementsByClassName("paywall") || [];
   const overlays = document.getElementsByClassName("overlay") || [];
-  const contentLimitation =
+  const contentsLimitation =
     document.getElementsByClassName("mv-content-limitation-fake-page") || [];
-  const blurContents = document.getElementsByClassName("t") || [];
-  const BannerSelector =
+  const bannerSelector =
     document.querySelectorAll('[class*="BannerSelector_banner"]') || [];
+  const viewsAnswerCTA =
+    document.querySelectorAll('[class*="ViewAnswerCTA"]') || [];
 
   for (const element of paywalls) {
     element.remove();
@@ -13,13 +15,29 @@ setInterval(() => {
   for (const element of overlays) {
     element.remove();
   }
-  for (const element of contentLimitation) {
+  for (const element of contentsLimitation) {
     element.remove();
   }
-  for (const element of BannerSelector) {
+  for (const element of bannerSelector) {
     element.remove();
   }
+  for (const element of viewsAnswerCTA) {
+    element.remove();
+  }
+
+  // Elements with blur to edit style
+  const blurContents = document.getElementsByClassName("t") || [];
+  const pdParagraphsMd =
+    document.querySelectorAll('[class*="pd-paragraph-md"]') || [];
+  const blockeds = document.getElementsByClassName("blocked") || [];
+
   for (const element of blurContents) {
     element.style.cssText += "filter: blur(0px) !important;";
+  }
+  for (const element of pdParagraphsMd) {
+    element.style.cssText += "filter: blur(0px) !important;";
+  }
+  for (const element of blockeds) {
+    element.style.cssText = "filter: blur(0px) !important;";
   }
 }, 3000);
